27c60fde82691287fff097743e4c59af74ec5ecc
ichitaso <willfeeltips@gmail.com>
2023-03-26 01:45:20 +0900

    v1.6.2
    - Added: AltStore 1.6.2
    - It is signed by permasigner and will work on iOS 14.0-15.4.1 even if you are not in jailbreak.

--- Makefile
+++ Makefile
@@ -1,21 +1,27 @@
 TARGET = AltStore
 ID = "com.ichitaso.altstore"
-VERSION = 1_4_9
-PKG_VER = 1.4.9
+VERSION = 1_6_2
+PKG_VER = 1.6.2
 
 .PHONY: all clean
 
 all: clean
-	wget https://cdn.altstore.io/file/altstore/apps/altstore/$(VERSION).ipa
-	unzip $(VERSION).ipa > /dev/null 2>&1
-	mkdir -p directry/Applications
-	mkdir -p directry/DEBIAN
-	cp -a Payload/$(TARGET).app directry/Applications/$(TARGET).app
-	cp -a control directry/DEBIAN/control
-	cp -a postinst directry/DEBIAN/postinst
-	chmod 755 directry/DEBIAN/postinst
+# https://github.com/permasigner/permasigner
+# https://permasigner.itsnebula.net/usage/run-on-macos
+	python3 -m permasigner -u https://cdn.altstore.io/file/altstore/apps/altstore/$(VERSION).ipa -o ./
+	mkdir deb-extract
+	dpkg -x $(TARGET)_$(PKG_VER).deb ./deb-extract
+	dpkg -e $(TARGET)_$(PKG_VER).deb ./deb-extract/DEBIAN
+	cp -a control deb-extract/DEBIAN/control
+	cp -a postinst deb-extract/DEBIAN/postinst
+	cp -a postrm deb-extract/DEBIAN/postrm
+	rm -f deb-extract/DEBIAN/prerm
+	chmod 755 deb-extract/DEBIAN/postinst
+	chmod 755 deb-extract/DEBIAN/postrm
+	ldid -s deb-extract/DEBIAN/postinst
 	find . -name ".DS_Store" | xargs rm
-	dpkg-deb -b -Zgzip directry $(ID)_$(PKG_VER)_iphoneos-arm.deb
+	fakeroot dpkg-deb -b -Zgzip deb-extract $(ID)_$(PKG_VER)_iphoneos-arm.deb
+	rm -rf deb-extract $(TARGET)_$(PKG_VER).deb
 
 clean:
-	rm -rf directry Payload ./*.ipa ./*.deb
+	rm -rf deb-extract Payload ./*.ipa ./*.deb
--- _postinst/postinst.mm
+++ _postinst/postinst.mm
@@ -1,7 +1,10 @@
 #import <UIKit/UIKit.h>
 #import <dlfcn.h>
+#import <spawn.h>
 
+#define APP_PATH @"/Applications/AltStore.app"
 #define INFO_PATH @"/Applications/AltStore.app/Info.plist"
+#define ROOT_PATH_NS(path)([[NSFileManager defaultManager] fileExistsAtPath:path] ? path : [@"/var/jb" stringByAppendingPathComponent:path])
 
 static CFStringRef (*$MGCopyAnswer)(CFStringRef);
 
@@ -12,12 +15,37 @@ static inline NSString *getUDID(void) {
     return (__bridge NSString *)(result);
 }
 
+extern char **environ;
+void run_action(const char *cmd) {
+    pid_t pid;
+    const char *argv[] = {"sh", "-c", cmd, NULL};
+    int status;
+    status = posix_spawn(&pid, "/bin/sh", NULL, NULL, (char * const *)argv, environ);
+    if (status == 0) {
+        printf("Child pid: %i\n", pid);
+        if (waitpid(pid, &status, 0) != -1) {
+            printf("Child exited with status %i\n", status);
+        } else {
+            perror("waitpid");
+        }
+    } else {
+        printf("posix_spawn: %s\n", strerror(status));
+    }
+}
+
 int main(int argc, char **argv, char **envp) {
     @autoreleasepool {
-        NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:INFO_PATH];
+        NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:ROOT_PATH_NS(INFO_PATH)];
         NSMutableDictionary *mutableDict = dict ? [dict mutableCopy] : [NSMutableDictionary dictionary];
         [mutableDict setValue:getUDID() forKey:@"ALTDeviceID"];
         [mutableDict writeToFile:INFO_PATH atomically:YES];
+
+        NSString *command1 = [NSString stringWithFormat:@"chown -R 0:0 %s", [ROOT_PATH_NS(APP_PATH) UTF8String]];
+        NSString *command2 = [NSString stringWithFormat:@"chmod -R 0755 %s", [ROOT_PATH_NS(APP_PATH) UTF8String]];
+        NSString *command3 = [NSString stringWithFormat:@"uicache -p %s", [ROOT_PATH_NS(APP_PATH) UTF8String]];
+        run_action([command1 UTF8String]);
+        run_action([command2 UTF8String]);
+        run_action([command3 UTF8String]);
     }
     return 0;
 }
